960e616ae864f984da7f485be245fe192c10c240
marcboso2005 <marcboso2005@gmail.com>
2025-12-16 19:48:04 +0100

    Començar a programar la pàgina inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+
+/*
+import { Routes } from '@angular/router';
+import { M01_SalutacioComponent } from './m01-salutacio.component';
+import { M02_SumaComponent } from './m02-suma.component';
+import { AlumnesComponent } from './alumnes';
+import { M04_PomodoroComponent } from './m04-pomodoro.component';
+import { M08_PipesComponent } from './m08-pipes.component';
+
+export const routes: Routes = [
+	{ path: '', redirectTo: 'suma', pathMatch: 'full' },
+	{ path: 'salutacio', component: M01_SalutacioComponent },
+	{ path: 'suma', component: M02_SumaComponent },
+	{ path: 'pomodoro', component: M04_PomodoroComponent },
+	{ path: 'pipes', component: M08_PipesComponent },
+	{ path: 'alumnes', component: AlumnesComponent },
+	{ path: '**', redirectTo: 'alumnes' }
+];
+
+*/ 
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,23 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+  // Exemple de main ts
+
+  /* 
+  import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+import { M01_SalutacioComponent } from './app/m01-salutacio.component';
+import { M02_SumaComponent } from './app/m02-suma.component';
+import { AlumnesComponent } from './app/alumnes';
+import { M04_PomodoroComponent } from './app/m04-pomodoro.component';
+import {M08_PipesComponent} from './app/m08-pipes.component';
+
+// Cambia el component que es carrega a l'inici de l'aplicació
+// per veure els diferents exemples
+// bootstrapApplication(App, appConfig)
+
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
+  */
